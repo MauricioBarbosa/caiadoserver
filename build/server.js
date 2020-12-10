928622a8c9bf5629ca1370e9d3982e4f965beaac
unknown <mauriciobarboza6.1@gmail.com>
2020-12-09 21:50:58 -0300

    commit

---
+++ build/server.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+const standalone_1 = require("@adonisjs/core/build/standalone");
+new standalone_1.Ignitor(__dirname)
+    .httpServer()
+    .start();
+//# sourceMappingURL=server.js.map
---
+++ build/start/routes.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Route_1 = __importDefault(require("@ioc:Adonis/Core/Route"));
+Route_1.default.on('/').render('welcome');
+//# sourceMappingURL=routes.js.map
